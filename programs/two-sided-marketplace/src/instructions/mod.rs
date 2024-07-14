0f978c81dd2154160601cc2a3b56b95cda8df383
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-15 03:18:22 +0530

    putting instructions together

---
+++ programs/two-sided-marketplace/src/instructions/mod.rs
@@ -0,0 +1,9 @@
+pub mod initialize_marketplace;
+pub mod list_service;
+pub mod purchase_service;
+pub mod resell_service;
+
+pub use initialize_marketplace::*;
+pub use list_service::*;
+pub use purchase_service::*;
+pub use resell_service::*;
